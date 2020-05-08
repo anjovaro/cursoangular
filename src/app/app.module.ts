f596b9bcc150caca68a5e7ea998174496ff3174e
Antonio J. Vazquez <anjovaro@gmail.com>
2020-05-09 00:47:22 +0200

    formulario reactivo completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CorreoComponent } from './Componets/correo/correo.component';
-import { ListaCorreoComponent } from './Componets/lista-correo/lista-correo.component';
+import { CorreoComponent } from './Components/correo/correo.component';
+import { ListaCorreoComponent } from './Components/lista-correo/lista-correo.component';
+import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.component';
 
 @NgModule({
-  declarations: [AppComponent, CorreoComponent, ListaCorreoComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CorreoComponent,
+    ListaCorreoComponent,
+    NuevoCorreoComponent,
+  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
