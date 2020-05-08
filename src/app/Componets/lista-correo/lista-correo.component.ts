cce78dffa5197c08921360145cb10f4b58829ab3
Antonio J. Vazquez <anjovaro@gmail.com>
2020-05-08 18:51:25 +0200

    nuevo componente lista-correo

---
+++ src/app/Componets/lista-correo/lista-correo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-correo',
+  templateUrl: './lista-correo.component.html',
+  styleUrls: ['./lista-correo.component.css']
+})
+export class ListaCorreoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './Componets/correo/correo.component';
+import { ListaCorreoComponent } from './Componets/lista-correo/lista-correo.component';
 
 @NgModule({
-  declarations: [AppComponent, CorreoComponent],
+  declarations: [AppComponent, CorreoComponent, ListaCorreoComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
