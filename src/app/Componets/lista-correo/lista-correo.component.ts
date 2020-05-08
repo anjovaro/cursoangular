00446ec4a1729f7f3aae57b2ab53d01203d0abf0
Antonio J. Vazquez <anjovaro@gmail.com>
2020-05-08 19:03:14 +0200

    Correos de prueba

--- src/app/Componets/lista-correo/lista-correo.component.ts
+++ src/app/Componets/lista-correo/lista-correo.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lista-correo',
   templateUrl: './lista-correo.component.html',
-  styleUrls: ['./lista-correo.component.css']
+  styleUrls: ['./lista-correo.component.css'],
 })
 export class ListaCorreoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    const correo1 = {
+      titulo: 'Titulo del 1',
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+      Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+      Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email`,
+      emisor: 'correoEmisor1@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: true,
+    };
+    const correo2 = {
+      titulo: 'Titulo del 2',
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+      Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor2@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    };
+    this.correos = [];
+    this.correos.push(correo1);
+    this.correos.push(correo2);
   }
 
+  ngOnInit(): void {}
 }
